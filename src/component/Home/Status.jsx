8334752def3463df1f51be335589c33bc09fc2fd
nimurr <nimurnerob404@gmail.com>
2026-01-11 12:01:50 +0600

    `Updated Status component with hover effects and added RevenueSubTrend component to DashboardHome page`

--- src/component/Home/Status.jsx
+++ src/component/Home/Status.jsx
@@ -4,7 +4,7 @@ import { FaArrowTrendUp, FaRegUser } from 'react-icons/fa6';
 const Status = () => {
     return (
         <div className='grid xl:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 gap-5'>
-            <div className='bg-[#1a3c58] p-5 rounded-lg flex justify-between items-start'>
+            <div className='bg-[#1a3c58] shadow duration-300 hover:shadow-orange-500 p-5 rounded-lg flex justify-between items-start'>
                 <div>
                     <p>Total User</p>
                     <h2 className='text-5xl font-semibold my-2'>1500</h2>
@@ -14,7 +14,7 @@ const Status = () => {
                     <FaRegUser className='text-[#1a3c58] text-2xl' />
                 </div>
             </div>
-            <div className='bg-[#1a3c58] p-5 rounded-lg flex justify-between items-start'>
+            <div className='bg-[#1a3c58] shadow duration-300 hover:shadow-orange-500 p-5 rounded-lg flex justify-between items-start'>
                 <div>
                     <p>Total User</p>
                     <h2 className='text-5xl font-semibold my-2'>1500</h2>
@@ -24,7 +24,7 @@ const Status = () => {
                     <FaRegUser className='text-[#1a3c58] text-2xl' />
                 </div>
             </div>
-            <div className='bg-[#1a3c58] p-5 rounded-lg flex justify-between items-start'>
+            <div className='bg-[#1a3c58] shadow duration-300 hover:shadow-orange-500 p-5 rounded-lg flex justify-between items-start'>
                 <div>
                     <p>Total User</p>
                     <h2 className='text-5xl font-semibold my-2'>1500</h2>
@@ -34,7 +34,7 @@ const Status = () => {
                     <FaRegUser className='text-[#1a3c58] text-2xl' />
                 </div>
             </div>
-            <div className='bg-[#1a3c58] p-5 rounded-lg flex justify-between items-start'>
+            <div className='bg-[#1a3c58] shadow duration-300 hover:shadow-orange-500 p-5 rounded-lg flex justify-between items-start'>
                 <div>
                     <p>Total User</p>
                     <h2 className='text-5xl font-semibold my-2'>1500</h2>
@@ -44,7 +44,7 @@ const Status = () => {
                     <FaRegUser className='text-[#1a3c58] text-2xl' />
                 </div>
             </div>
-            <div className='bg-[#1a3c58] p-5 rounded-lg flex justify-between items-start'>
+            <div className='bg-[#1a3c58] shadow duration-300 hover:shadow-orange-500 p-5 rounded-lg flex justify-between items-start'>
                 <div>
                     <p>Total User</p>
                     <h2 className='text-5xl font-semibold my-2'>1500</h2>
--- src/page/DashboardHome/DashboardHome.jsx
+++ src/page/DashboardHome/DashboardHome.jsx
@@ -1,3 +1,4 @@
+import RevenueSubTrend from "../../component/Home/RevenueSubTrend";
 import Status from "../../component/Home/Status";
 
 const DashboardHome = () => {
@@ -5,6 +6,9 @@ const DashboardHome = () => {
     <section>
       <h1 className="text-4xl font-semibold py-5 px-3">Overview</h1>
       <Status />
+      <div>
+        <RevenueSubTrend />
+      </div>
     </section>
   );
 };
